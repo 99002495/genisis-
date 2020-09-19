7ca687401e78ca93fb52818c53bfcbab85a9ea1c
Prajwal rao p <71481375+99002495@users.noreply.github.com>
2020-09-19 10:14:34 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,86 @@
-#include <calculator_operations.h>
-
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
 
 
-/* Start of the application */
-int main(int argc, char *argv[])
+#include "calculator.h"
+#include "math.h"
+#include"stdio.h"
+
+
+
+
+/* input 2 number with choice of opperator*/
+/*this programme uses function Pointer array */
+int calci(int num1,int num2,int choice)
 {
-    printf("\n****Calculator****\n");
-    while(1)
-    {
-        calculator_menu();
-    }
+
+
+
+int add(int num1, int num2);
+int subtract(int num1, int num2);
+int multiply(int num1, int num2);
+int divide(int num1, int num2);
+int power(int num1,int num);
+int root(int num1,int num);
+int logarithm(int num1,int num);
+
+
+
+  int  result;
+  int (*op[7])(int, int);
+
+  op[0] = add;//choice 1 for add
+  op[1] = subtract;//choice 2 for sub
+  op[2] = multiply;//choice 3 for multiplication
+  op[3] = divide;// choice 4 for divide
+  op[4] = power;//choice 5 for num1 power num 2
+  op[5] = logarithm;//choice 6 for log num1 to base num2
+  op[6] = root;
+
+
+  result = op[choice](num1, num2);
+
+
+  return result;
 }
 
-void calculator_menu(void)
+float advance_calci(float num1,int choice)
 {
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     __fpurge(stdin);
-    scanf("%d", &calculator_operation);
+    float square_root(float num1);
+    float log_to_10(float num1);
+    float natural_log(float num1);
+    float sine_fun(float num1);
+    float cos_fun(float num1);
+    float sec_fun(float num1);
+    float cosec_fun(float num1);
+    float exponential(float num1);
+    float factorial(float number);
+    float one_by(float number);
+    float modulous(float number);
 
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
+    float result;
+    float (*opa[12])(float);
+    opa[0] = square_root;
+  opa[1] = log_to_10;
+  opa[2] = natural_log;
+  opa[3] = sine_fun;
+  opa[4] = sec_fun;
+  opa[5] = cosec_fun;
+  opa[7] = factorial;
+  opa[8] = one_by;
+  opa[9] = modulous;
+  opa[10]= exponential;
+  opa[11]= cos_fun;
+    result = opa[choice](num1);
+    return result;
 
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
 }
 
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+int matrix(){
+    int matrixs_sum();
+    int matrix_sub();
+    int matrix_mul();
+    int (*opm[3])();
+    opm[0]=matrixs_sum;
+    opm[1]=matrix_sub;
+    opm[3]=matrix_mul;
+    return 0;
 }
